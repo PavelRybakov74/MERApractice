5a58d0ee917b72b8e02ca3bf00ae68c039a11884
PavelRybakov74 <pasha.rybakov.2002@mail.ru>
2023-07-25 00:25:32 +0300

    v1.1

--- Shop/Program.cs
+++ Shop/Program.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
 
-namespace Shop {
-    public class Program { 
-        public static void Main(string[] args) {
-            CreateWebHostBuilder(args).Build().Run();
+namespace Shop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = CreateWebHostBuilder(args);
+            builder.Build().Run();
         }
 
-        private static IWebHostBuilder CreateWebHostBuilder(string[] args) => 
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
     }
 }
-
